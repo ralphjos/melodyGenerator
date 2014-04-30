b5e39013c9a9353f75f317958da8a6483bac8de0
Ralph Arroyo <ralphjos@berkeley.edu>
2014-04-30 12:40:20 -0700

    updated melodyGenerator; kinda junky

--- melodyGenerator.py
+++ melodyGenerator.py
@@ -2,6 +2,10 @@ from music21 import *
 import util
 
 melodies = []
+threeFour = 0
+fourFour = 0
+twoTwo = 0
+twoFour = 0
 
 def getKey(stream):
     """
@@ -18,6 +22,9 @@ def getKey(stream):
     pitchAndMode = music21KeySignature.elements[0].pitchAndMode
     return pitchAndMode
 
+def getTimeSignature(stream):
+    music21TimeSignature = stream.flat.getElementsByClass(meter.TimeSignature)
+    return music21TimeSignature
 
 def getScaleDegree(pitchAndMode, note):
     """
@@ -27,13 +34,11 @@ def getScaleDegree(pitchAndMode, note):
     mode = pitchAndMode[1]
     ref_scale = None
     if pitchAndMode[1] == "major":
-        ref_scale = scale.MinorScale(pi)
+        ref_scale = scale.MajorScale(pi)
     else:
         ref_scale = scale.MinorScale(pi)
     degree = ref_scale.getScaleDegreeAndAccidentalFromPitch(pitch.Pitch(note.name))
     return degree
-    
-    
 
 def loadMelodies():
     """
@@ -41,25 +46,47 @@ def loadMelodies():
     For some reason, chorales 274, 275, and 409 are 
     missing from the corpus, hence the need for multiple loops
     """
-    for i in range(250, 251):
-        chorale = corpus.parse('bach/bwv' + str(i))
-        melodies.append(chorale.getElementById('Soprano')) 
     """
-    for i in range(276, 409):
+    for i in range(250, 274):
         chorale = corpus.parse('bach/bwv' + str(i))
-        melodies.append(chorale.getElementById('Soprano'))
+        melody = chorale.getElementById('Soprano')
+        if melody != None:
+            melodies.append(melody)
+        else:
+            melodies.append(chorale.getElementById('S.'))
+    """
+    for i in range(278, 409):
+        chorale = corpus.parse('bach/bwv' + str(i))
+        melody = chorale.getElementById('Soprano')
+        if melody != None:
+            melodies.append(melody)
+            continue
+        melody = chorale.getElementById('S.')
+        if melody != None:
+            melodies.append(melody)
+            continue
+        melody = chorale.getElementsByClass(stream.Part)[0]
+        melodies.append(melody)
         
     for i in range(410, 439):
         chorale = corpus.parse('bach/bwv' + str(i))
         melodies.append(chorale.getElementById('Soprano'))
-    """
 
 def run():
-    loadMelodies()
-    print 'There are ' + str(len(melodies)) + ' melodies ready for use!\n'   
+    #loadMelodies()
+    chorale = corpus.parse('bach/bwv250')
+    melody = chorale.getElementsByClass(stream.Part)[0]
+    melody.show('musicxml')
+    melody.show('text')
+    #getKey(chorale)
+    #print 'There are ' + str(len(melodies)) + ' melodies ready for use!\n'   
     # If you set up your music21 environment correctly, you can see that the first melody is the soprano part from BWV 250.
     # melodies[0].show('musicxml')
-    melodies[0].show('text')
-    print getScaleDegree(getKey(melodies[0]), note.Note(''))
+    # i = 0
+    
+    #while (i < len(melodies)):
+    #    print i, getKey(melodies[i])
+    #    i += 1
+    #print getScaleDegree(getKey(melodies[0]), note.Note('C4'))
     
 run()
